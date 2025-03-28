52cd04b6f1d34618031e50633fc28271ae63a55b
Satyam Bansal <sbansal1999@gmail.com>
2025-03-28 20:00:18 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import express from "express";
+import dotenv from "dotenv";
+import { Resend } from "resend";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+app.get("/scrape", async (req, res) => {
+  try {
+    const myHeaders = new Headers();
+    myHeaders.append("authorization", "Bearer " + process.env.TICKET_GENIE_API_KEY);
+
+    const requestOptions = {
+      method: "GET",
+      headers: myHeaders,
+    };
+
+    fetch("https://rcbmpapi.ticketgenie.in/ticket/standslist/2", requestOptions)
+      .then((response) => response.json())
+      .then(async (result) => {
+        console.log(result);
+        const eventName = result.result.event_Name;
+        const stands = result.result.stands;
+
+        if (stands.length === 1) {
+          await resend.emails.send({
+            from: "Ticket Tracker<onboarding@resend.dev>",
+            to: "sbansal1999@gmail.com",
+            subject: "CHECK TICKETS ASAP",
+            text: "CHECK TICKETS ASAP",
+          });
+        }
+        console.log(eventName);
+        console.log(stands);
+      });
+
+    res.json("data");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+export { app };
